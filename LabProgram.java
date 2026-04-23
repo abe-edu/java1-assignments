c98f18b568240d68f2e7881597dc58f64a1373c4
abe-edu <abe.s.molina.ed@gmail.com>
2026-04-23 23:11:43 +0000

    Complete word frequency lab

--- LabProgram.java
+++ LabProgram.java
@@ -1,37 +1,31 @@
 import java.util.Scanner;
 
 public class LabProgram {
-   
-   public static int fibonacci(int n) {
-    //Requirement for negative numbers
-        if (n < 0) {
-        return -1;
-        }
 
-    //Base cases 0, 1
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-
-    //New number = Previous number + Previous previous number
-        int a = 0;
-        int b = 1; 
-        int c = 0;
-
-    //for loop, count up until i reaches n
-        for (int i = 2; i <= n; ++i) {
-            c = a + b;
-            a = b;
-            b = c; 
-        }
-
-        return b;  
+   public static int getWordFrequency(String[] wordsList, int listSize, String currWord) {
+      int count = 0;
+      for (int i = 0; i < listSize; i++) {
+         if (wordsList[i].equalsIgnoreCase(currWord)) {
+            count++;
+         }
+      }
+      return count;
    }
-   
+
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int startNum;
-      
-      startNum = scnr.nextInt();
-      System.out.println("fibonacci(" + startNum + ") is " + fibonacci(startNum));
+      int numWords;
+      String[] words = new String[20]; // Maximum size as per requirements
+
+      numWords = scnr.nextInt();
+
+      for (int i = 0; i < numWords; i++) {
+         words[i] = scnr.next();
+      }
+
+      for (int i = 0; i < numWords; i++) {
+         int frequency = getWordFrequency(words, numWords, words[i]);
+         System.out.println(words[i] + " " + frequency);
+      }
    }
 }
